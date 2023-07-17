bd5240f3bdfb293b25c983d2fcb99ec9b17e9f63
Julia <avokdorog@mail.ru>
2023-07-17 18:52:35 +0300

    cheange src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Здравствуйте!");
+
+        int customerAccount = 100; // изначальная сумма на счету
+        int refill = 1100; // пополнение счёта
+        int bonus;
+
+        if (refill > 1000) {
+            bonus = refill / 100;
+        }
+        else {
+            bonus = 0;
+        }
+        int balance = customerAccount + refill +  bonus;
+        System.out.println("Ваш баланс: "  + balance + " руб. Ваш бонус: "+ bonus + "руб.");
+
+        }
+    }
+
+
